8135b703b3156301c7b374803e6e057f59df1aa6
Muhammed Qadan <muhammedqadan@muhammeds-mbp.home>
2019-03-04 16:10:13 -0800

    initial commit of project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function(){
+   var header = document.getElementById("header");
+   var menuToggle = document.getElementById("toggle");
+    
+    
+    menuToggle.addEventListener("click",function(){
+        if(header.className === "open"){
+            header.className = "";
+        }
+        else{
+             header.className = "open";
+        }
+    })
+    
+})();
